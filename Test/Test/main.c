86b21fd350ae7ca01121d35bc8944d3149383c99
olvyshh <olvyshh@gmail.com>
2021-06-20 00:03:10 +0300

    Изменение

---
+++ Test/Test/main.c
@@ -0,0 +1,23 @@
+/*
+ * Test.c
+ *
+ * Created: 19.06.2021 22:43:08
+ * Author : steam
+ */ 
+
+#include <avr/io.h>
+volatile int i=0;
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		i++;
+		i++;
+				i++;		
+
+
+    }
+}
+
